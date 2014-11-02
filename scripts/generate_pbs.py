e8c7cb84b42cfc635ec1313cb70051bbbc79dc05
Matthew Burke <matthelb@usc.edu>
2014-11-01 20:08:17 -0700

    Add support for Python 2

--- scripts/generate_pbs.py
+++ scripts/generate_pbs.py
@@ -1,30 +1,34 @@
+from __future__ import print_function
+
 import math
 import sys
 
-if len(sys.argv) != 9:
-	print('Usage: {0} <num_cities> <max_coord> <iterations> <trials> <processors> <walltime> <exec_dir> <results_dir>'
+if len(sys.argv) != 10:
+	print('Usage: {0} <min_coord> <max_coord> <trials> <processors> <walltime> <exec_dir> <results_dir> <input_file> <cairomm_path>'
 				.format(sys.argv[0]), file=sys.stderr)
 	sys.exit(1)
 
 PROCESSORS_PER_NODE = 8
 
-num_cities = int(sys.argv[1])
+min_coord = float(sys.argv[1])
 max_coord = float(sys.argv[2])
-iterations = int(sys.argv[3])
-trials = int(sys.argv[4])
-processors = int(sys.argv[5])
-walltime = sys.argv[6]
-exec_dir = sys.argv[7]
-results_dir = sys.argv[8]
+trials = int(sys.argv[3])
+processors = int(sys.argv[4])
+walltime = sys.argv[5]
+exec_dir = sys.argv[6]
+results_dir = sys.argv[7]
+input_file = sys.argv[8]
+cairomm_path = sys.argv[9]
 
-results_dir += '/n{0}'.format(num_cities)
-real_iterations = math.floor(iterations / processors)
+real_trials = math.floor(trials / processors)
 
 for i in range(processors + 1):
 	f = open('job{0}.pbs'.format(i), 'w+')
 	print('#!/bin/bash', file=f)
 	print('#PBS -l nodes=1:ppn=1', file=f)
 	print('#PBS -l walltime={0}'.format(walltime), file=f)
-	itrs = min(iterations - real_iterations * i, real_iterations)
-	print('{0}/generate_tsp_csv {1} 0 {2} {3} {4}'.format(exec_dir, num_cities, max_coord, itrs, trials), file=f)
+	trial_start = i * real_trials
+	trial_end = min((i + 1) * real_trials, trials)
+	print('export LD_LIBRARY_PATH="{0}:$LD_LIBRARY_PATH"'.format(cairomm_path), file=f)
+	print('{0}/generate_tsp_csv {1} {2} {3} {4} {5} {6} {7} {8}'.format(exec_dir, results_dir, 1, min_coord, max_coord, trial_start, trial_end, input_file, i), file=f)
 	f.close()
