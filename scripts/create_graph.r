c23b3956aa8b0d874d61ed18e45a3215be1e3566
Matthew Burke <matthelb@usc.edu>
2014-12-29 13:10:13 -0500

    Update R script for new output directory format

--- scripts/create_graph.r
+++ scripts/create_graph.r
@@ -1,11 +1,15 @@
+library(tools)
+
 f_n <- function(data_frame) {
   return (abs((data_frame$T.../1000 - data_frame$T../1000) - (data_frame$T./1000 - data_frame$T/1000))) 
 }
 
-read_csvs <- function(n, dir, x, y) {
+read_csvs <- function(dir) {
+  x <- c()
+  y <- c()
   for (file in list.files(dir)) {
     dat <- read.csv(file)
-    x <- c(x, n)
+    x <- c(x, as.integer(basename(file_path_sans_ext(file))))
     y <- c(y, mean(f_n(dat)))
   }
   return (list(x, y))
