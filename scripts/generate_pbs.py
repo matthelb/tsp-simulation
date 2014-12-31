27e38e04d9cbf097932da7c6d6201437da330a77
Matthew Burke <matthelb@usc.edu>
2014-12-30 23:34:08 -0500

    Update scripts to use processors argument when calling simulation exec

--- scripts/generate_pbs.py
+++ scripts/generate_pbs.py
@@ -2,46 +2,43 @@ from __future__ import print_function
 
 import math
 import sys
+import os
 
-if len(sys.argv) != 14:
-	print('Usage: {0} <num_cities> <min_coord> <max_coord> <trials> <processors> <walltime> <exec_dir> <results_dir> <input_file> <cairomm_path> <output_dir> <maxchunksize> <run_script>'
+if len(sys.argv) != 16:
+	print('Usage: {0} <run_script> <cairomm_path> <exec_file> <output_dir> <iterations> <min_coord> <max_coord> <trials> <input_file> <max_compute_time> <max_chunk_size> <processors> <processors_per_trial> <walltime> <pbs_output_dir>'
 				.format(sys.argv[0]), file=sys.stderr)
 	sys.exit(1)
 
 PROCESSORS_PER_NODE = 8
 
-num_cities = int(sys.argv[1])
-min_coord = float(sys.argv[2])
-max_coord = float(sys.argv[3])
-trials = int(sys.argv[4])
-processors = int(sys.argv[5])
-walltime = sys.argv[6]
-exec_dir = sys.argv[7]
-results_dir = sys.argv[8]
+run_script = sys.argv[1]
+cairomm_path = sys.argv[2]
+exec_file = sys.argv[3]
+output_dir = sys.argv[4]
+iterations = int(sys.argv[5])
+min_coord = float(sys.argv[6])
+max_coord = float(sys.argv[7])
+trials = int(sys.argv[8])
 input_file = sys.argv[9]
-cairomm_path = sys.argv[10]
-output_dir = sys.argv[11]
-maxchunksize = int(sys.argv[12])
-run_script = sys.argv[13]
+max_compute_time = int(sys.argv[10])
+max_chunk_size = int(sys.argv[11])
+processors = int(sys.argv[12])
+processors_per_trial = int(sys.argv[13])
+walltime = sys.argv[14]
+pbs_output_dir = sys.argv[15]
 
 nodes = int(math.ceil(processors / PROCESSORS_PER_NODE))
-real_trials = int(trials / processors)
+trial_groups = processors / processors_per_trial
+trials_per_group = int(math.ceil(trials / trial_groups))
 
-f = open('{0}/job{1}.pbs'.format(output_dir, num_cities), 'w+')
+out_file = os.path.splitext(os.path.basename(input_file))[0]
+
+f = open('{0}/{1}.pbs'.format(pbs_output_dir, out_file), 'w+')
 print('#!/bin/bash', file=f)
 print('#PBS -l nodes={0}:ppn={1}'.format(max(1, nodes), min(PROCESSORS_PER_NODE, processors)), file=f)
 print('#PBS -l walltime={0}'.format(walltime), file=f)
-for i in range(1, nodes + 1):
-	print('pbsdsh -v -n {0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12}'.format(i, run_script, cairomm_path, exec_dir, results_dir, 1, min_coord, max_coord, trials, real_trials, input_file, 0, maxchunksize), file=f)
-f.close()
-
-# for i in range(processors + 1):
-# 	f = open('{0}/job{1}_{2}.pbs'.format(output_dir, str(i).zfill(int(math.log10(processors)) + 1), num_cities), 'w+')
-# 	print('#!/bin/bash', file=f)
-# 	print('#PBS -l nodes=1:ppn=1', file=f)
-# 	print('#PBS -l walltime={0}'.format(walltime), file=f)
-# 	trial_start = int(i * real_trials)
-# 	trial_end = int(min((i + 1) * real_trials, trials))
-# 	print('export LD_LIBRARY_PATH="{0}:$LD_LIBRARY_PATH"'.format(cairomm_path), file=f)
-# 	print('{0}/generate_tsp_csv {1} {2} {3} {4} {5} {6} {7} {8}'.format(exec_dir, results_dir, 1, min_coord, max_coord, trial_start, trial_end, input_file, i), file=f)
-# 	f.close()
+for i in range(trial_groups):
+	node = int(i * processors_per_trial / PROCESSORS_PER_NODE)
+	trials_start = i * trials_per_group
+	trials_end = (i + 1) * trials_per_group
+	print('pbsdsh -v -n {0} {1} {2} {3} {4} {5} {6} {7} {8} {9} {10} {11} {12} {13}'.format(node, run_script, cairomm_path, exec_file, output_dir, iterations, min_coord, max_coord, trials_start, trials_end, input_file, max_compute_time, max_chunk_size, processors_per_trial), file=f)
